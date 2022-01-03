1783cd926a1446f7297bcf90083ae1cdc5af6ff8
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2022-01-03 12:11:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+status = "";
 
+text_input_value = "";
 function setup(){
-    canvas = createCanvas(350,350);
+    canvas = createCanvas(480,380);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(380,380);
+    video.size(480,380);
     video.hide();
-     
+    
+}
+function draw() {
+    image (video,0,0,480,380);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status: Detecting object";
+    text_input_value = document.getElementById("search").value;
+
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    
 }
